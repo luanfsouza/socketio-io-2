7239f318c58906ea37a2c1ebf2080471a835b3b7
luanfsouza <luanferreira7777@gmail.com>
2022-08-12 10:44:36 -0300

    u

--- index.js
+++ index.js
@@ -25,38 +25,38 @@ io.on('connection', client =>{
 //   },
 // });
 
-// let users = []
-// const addUser = (userId, socketId)=>{
-//     !users.some(user=>user.userId == userId) && users.push({userId, socketId})
-// }
-// const removeUser = (socketId)=>{
-//     users = users.filter(user=>user.socketId != socketId)
-// }
-// const getUser = (userId)=>{
-//     return users.find(user=>user.userId == userId)
-// }
-// console.log(users)
-// // io.on("connection", (socket) => {
-// //     //when connect
-// //   console.log("user connected na porta: " + PORT);
-// //   //take userId and socketId from user
-// //   socket.on('addUser', userId=>{
-// // addUser(userId, socket.id)
-// // io.emit("getUsers", users)
-// //   })
+let users = []
+const addUser = (userId, socketId)=>{
+    !users.some(user=>user.userId == userId) && users.push({userId, socketId})
+}
+const removeUser = (socketId)=>{
+    users = users.filter(user=>user.socketId != socketId)
+}
+const getUser = (userId)=>{
+    return users.find(user=>user.userId == userId)
+}
+console.log(users)
+io.on("connection", (socket) => {
+    //when connect
+  console.log("user connected na porta: " + PORT);
+  //take userId and socketId from user
+  socket.on('addUser', userId=>{
+addUser(userId, socket.id)
+io.emit("getUsers", users)
+  })
 
-// //   //send get message
-// //   socket.on("sendMessage",({senderId, receiverId, text})=>{
-// // const user = getUser(receiverId)
-// // io.to(user.socketId).emit('getMessage',{
-// //     senderId,
-// //     text,
-// // })
-// //   })
-// //   //when disconnect
-// // socket.on('disconnect',()=>{
-// //     console.log('use disconect')
-// //     removeUser(socket.id)
-// // })
+  //send get message
+  socket.on("sendMessage",({senderId, receiverId, text})=>{
+const user = getUser(receiverId)
+io.to(user.socketId).emit('getMessage',{
+    senderId,
+    text,
+})
+  })
+  //when disconnect
+socket.on('disconnect',()=>{
+    console.log('use disconect')
+    removeUser(socket.id)
+})
 
-// // });
+});
